1a4864a7dead761f911c4fdf4bf18ac2457b0997
bruno <bpires.siqueira@gmail.com>
2025-08-28 21:07:14 -0300

    feat: gestao escolar

--- Aula-2/gestao_escolar.py
+++ Aula-2/gestao_escolar.py
@@ -1,36 +1,28 @@
 class Escola:
-    def __init__(self, nome="", idade=0, nota1=0.0, nota2=0.0):
+    def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
-        self.nota1 = nota1
-        self.nota2 = nota2
+        self.nota1 = None
+        self.nota2 = None
         self.nota_final = None
 
-    def inscricao(self):
-        self.nome = input("Nome do aluno: ")
-        self.idade = int(input(f"Idade do {self.nome}: "))
+    def apresentar(self):
+        print(f"O aluno {self.nome}")
 
-    def nota(self):
-        nota1 = float(input('N1: '))
-        nota2 = float(input('N2: '))
-        self.nota1 = nota1
-        self.nota2 = nota2
+    def nota(self, nota_1, nota_2):
+        self.nota1 = nota_1
+        self.nota2 = nota_2
+
+    def nota(self, nota_1, nota_2):
+        self.nota1 = nota_1
+        self.nota2 = nota_2
 
     def calcular(self):
         self.nota_final = (self.nota1 + self.nota2) / 2
-    
-        variavel = ''
 
         if self.nota_final >= 7:
-            variavel = f"Aluno {self.nome} aprovado {self.nota_final}!"
+            print(f"Aluno {self.nome} aprovado {self.nota_final}!")
         elif self.nota_final < 7 and self.nota_final >= 4:
-            variavel = f"O aluno {self.nome} está em recuperação {self.nota_final}."
+            print(f"O aluno {self.nome} está em recuperação {self.nota_final}.") 
         else:
-            variavel = f"Aluno {self.nome} Reprovado {self.nota_final}!"
-
-        return variavel
-
-aluno = Escola()
-aluno.inscricao()
-aluno.nota()
-print(aluno.calcular())
+            print(f"Aluno {self.nome} Reprovado {self.nota_final}!")
--- Aula-2/main.py
+++ Aula-2/main.py
@@ -1,8 +1,16 @@
 from produto import Produto as prod
+from gestao_escolar import Escola as scho
 
 produto1 = prod("Leite", 20.00, 40)
 produto2 = prod("Maça", 9.99, 30)
 produto3 = prod("Água", 3.99, 50)
 
-for i in [produto1, produto2, produto3]:
-    i.apresentacao()
+aluno1 = scho('Zé da manga', 25)
+aluno2 = scho('Zé das Coves', 30)
+aluno3 = scho('Maria da Manga', 45)
+
+listaP = [produto1, produto2, produto3]
+listaA = [aluno1, aluno2, aluno3]
+
+for i in range(3):
+    listaP[i].apresentacao(), print('Comprou'), listaA[i].apresentar(), print('apresentacao')
